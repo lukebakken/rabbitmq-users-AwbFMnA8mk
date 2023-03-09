f5eae700bb618f23ebc47df6fa1f1352f6d9b3a5
Luke Bakken <luke@bakken.io>
2023-03-09 09:18:17 -0800

    churn tweaks

--- churn.py
+++ churn.py
@@ -6,7 +6,6 @@ import threading
 import pika
 import random
 import socket
-import time
 
 LOG_FORMAT = (
     "%(levelname) -10s %(asctime)s %(name) -30s %(funcName) "
@@ -16,13 +15,11 @@ LOGGER = logging.getLogger(__name__)
 
 logging.basicConfig(level=logging.ERROR, format=LOG_FORMAT)
 
-stopping = False
-
 threads = []
 
 
-def do_work_0(i):
-    while True and not stopping:
+def do_work_0(stop_event: threading.Event, i: int):
+    while not stop_event.is_set():
         thread_id = threading.get_ident()
         LOGGER.info("i: %s pika thread id: %s", i, thread_id)
         credentials = pika.PlainCredentials("guest", "guest")
@@ -30,34 +27,40 @@ def do_work_0(i):
         parameters = pika.ConnectionParameters(
             host="localhost", port=p, credentials=credentials, heartbeat=5
         )
-        connection = pika.BlockingConnection(parameters)
-        # for i in range(1, random.randrange(5, 10)):
-        #    connection.channel()
-        connection.process_data_events(time_limit=random.randrange(5, 10))
-        connection.close()
+        connection = None
+        try:
+            connection = pika.BlockingConnection(parameters)
+            for i in range(1, random.randrange(5, 10)):
+                connection.channel()
+            connection.process_data_events(time_limit=random.randrange(5, 10))
+        finally:
+            if connection is not None:
+                connection.close()
 
 
-def do_work_1(i):
+def do_work_1(stop_event: threading.Event, i: int):
     thread_id = threading.get_ident()
     LOGGER.info("i: %s socket thread id: %s", i, thread_id)
-    while True and not stopping:
+    while not stop_event.is_set():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(("localhost", 5672))
-        time.sleep(random.randrange(0, 10))
+        stop_event.wait(random.randrange(0, 10))
         s.close()
 
 
-for i in range(0, 8):
-    t0 = threading.Thread(target=do_work_0, args=(i,))
+stop_event = threading.Event()
+
+for i in range(0, 32):
+    t0 = threading.Thread(target=do_work_0, args=(stop_event, i))
     t0.start()
     threads.append(t0)
-    t1 = threading.Thread(target=do_work_1, args=(i,))
+    t1 = threading.Thread(target=do_work_1, args=(stop_event, i))
     t1.start()
     threads.append(t1)
 
 input("ANY KEY TO STOP")
 print()
-stopping = True
+stop_event.set()
 # Wait for all to complete
 for thread in threads:
     thread.join()
